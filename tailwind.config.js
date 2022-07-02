e4346996158871a9e5bdbe4e3961d65d1a49b9ab
Pratham Sitoula <psitoula2003@gmail.com>
2022-07-01 20:53:40 -0600

    Finished desktop Nav

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,17 @@
 module.exports = {
   content: ["./*.html"],
   theme: {
+    screens: {
+      md: "800px",
+    },
     extend: {
       fontFamily: {
         sans: ["Open Sans", "sans-serif"],
       },
       colors: {
-        DarkBlue: "hsl(233, 26%, 24%)",
+        darkBlue: "hsl(233, 26%, 24%)",
         limeGreen: "hsl(136, 65%, 51%)",
-        BrightCyan: "hsl(192, 70%, 51%)",
+        brightCyan: "hsl(192, 70%, 51%)",
         grayishBlue: "hsl(233, 8%, 62%)",
         lightGrayishBlue: "hsl(220, 16%, 96%)",
         veryLightGray: "hsl(0, 0%, 98%)",
