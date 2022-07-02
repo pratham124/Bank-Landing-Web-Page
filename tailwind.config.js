eca648669a8508a7448767d20ea2157001d841a9
Pratham Sitoula <psitoula2003@gmail.com>
2022-07-01 20:20:58 -0600

    Navigation first element

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./*.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Open Sans", "sans-serif"],
+      },
+      colors: {
+        DarkBlue: "hsl(233, 26%, 24%)",
+        limeGreen: "hsl(136, 65%, 51%)",
+        BrightCyan: "hsl(192, 70%, 51%)",
+        grayishBlue: "hsl(233, 8%, 62%)",
+        lightGrayishBlue: "hsl(220, 16%, 96%)",
+        veryLightGray: "hsl(0, 0%, 98%)",
+      },
+    },
   },
   plugins: [],
 };
